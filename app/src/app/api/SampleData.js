451f39f82572ae1f9c7c78187b1b51cd4f2db557
Donaldo Gómez <carlos.ramon-gomez@inetum.com>
2023-01-04 18:12:51 -0600

    Recursos

---
+++ app/src/app/api/SampleData.js
@@ -0,0 +1,48 @@
+export const sampleData = [
+    {
+        id: 1,
+        title: 'Viaje al museo La Venta',
+        date: '2023-03-21',
+        category: 'cultura',
+        description: 'Museo al aire libre con cabezas de piedra y altares antiguos, así como un zoológico con jaguares y más.',
+        city: 'Villahermosa, Tabasco, México',
+        venue: 'Boulevard Adolfo Ruiz Cortines S/N, Jesus Garcia, 86040 Villahermosa, Tab., México',
+        hostedBy: 'Secretaria de cultura',
+        hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
+        attendees: [
+            {
+                id: 1,
+                name: 'Alicia',
+                photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+            },
+            {
+                id: 2,
+                name: 'Pedro',
+                photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: 'Ascenso al cerro de la pava',
+        date: '2023-03-18',
+        category: 'deportes, ecoturismo',
+        description: 'Sube el cerro de la pava, una elevación de 990m localizada entre Tabasco y Chiapas. Descubre la vista desde el pico en su cima, un escenario que jamas olvidaras!',
+        city: 'Huimanguillo, Tabasco, México',
+        venue: 'Centro turistico Agua Selva, Villa Guadalupe, Huimanguillo, Tabasco',
+        hostedBy: 'Ecoturismo Olmaya',
+        hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
+        attendees: [
+            {
+                id: 1,
+                name: 'Alicia',
+                photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
+            },
+            {
+                id: 2,
+                name: 'Pedro',
+                photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
+            }
+        ]
+    }
+];
